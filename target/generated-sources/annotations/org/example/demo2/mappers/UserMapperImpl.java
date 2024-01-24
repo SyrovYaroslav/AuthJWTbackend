e93da42a68db4194ca6c1dbc4d13699e85b7e31c
Yaroslav <syrovatkaaroslav@gmail.com>
2024-01-24 11:07:54 +0200

    some configv3

---
+++ target/generated-sources/annotations/org/example/demo2/mappers/UserMapperImpl.java
@@ -0,0 +1,47 @@
+package org.example.demo2.mappers;
+
+import javax.annotation.processing.Generated;
+import org.example.demo2.dto.SignUpDto;
+import org.example.demo2.dto.UserDto;
+import org.example.demo2.entitys.User;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-01-24T10:56:41+0200",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.9 (Amazon.com Inc.)"
+)
+@Component
+public class UserMapperImpl implements UserMapper {
+
+    @Override
+    public UserDto toUserDto(User user) {
+        if ( user == null ) {
+            return null;
+        }
+
+        UserDto.UserDtoBuilder userDto = UserDto.builder();
+
+        userDto.id( user.getId() );
+        userDto.firstName( user.getFirstName() );
+        userDto.lastName( user.getLastName() );
+        userDto.login( user.getLogin() );
+
+        return userDto.build();
+    }
+
+    @Override
+    public User signUpToUser(SignUpDto signUpDto) {
+        if ( signUpDto == null ) {
+            return null;
+        }
+
+        User.UserBuilder user = User.builder();
+
+        user.firstName( signUpDto.firstName() );
+        user.lastName( signUpDto.lastName() );
+        user.login( signUpDto.login() );
+
+        return user.build();
+    }
+}
